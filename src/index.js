7ccf3bec05fef3c53ef0132ee94de4a9e716f79e
ilia <ilyakut@gmail.com>
2020-06-13 14:34:01 +0300

    added app.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log(211);
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
